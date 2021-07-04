3c4fc4d7b8506ff28232880a6f34734be746b358
Nursurtan <arapovnur23@gmail.com>
2021-07-04 07:52:35 +0700

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by нурсултан арапов on 16.06.2021.
+//
+
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
